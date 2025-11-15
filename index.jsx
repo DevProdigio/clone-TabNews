b107225f69b18dcc01efbc51243d14f1218ccdb9
Pedro Paulo Carneiro Ferreira <158389145+DevProdigio@users.noreply.github.com>
2025-11-15 16:33:10 +0000

    Desafio 1

--- index.jsx
+++ index.jsx
@@ -1,3 +1,11 @@
-<p>Olá Buildora</p>
+function App() {
+    return (
+        <div>
+        <h1>Tome em seu cu viado fela da pouta</h1>
+        <p>Espero que você pegue uma infecção fudida e morra seu verme miseravel</p>
+        <p>e tome no seu cu</p>
+    </div>
+    )
+}
 
 export default index.jsx;
