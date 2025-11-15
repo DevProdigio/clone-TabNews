8701e85627a8037eea9ce3294933eda0930ac055
Pedro Paulo Carneiro Ferreira <158389145+DevProdigio@users.noreply.github.com>
2025-11-15 16:42:15 +0000

    Agora sim desafio 1 completo

---
+++ home.js
@@ -0,0 +1,11 @@
+function home(){
+    return (
+        <div>
+        <h1>Tome em seu cu viado fela da pouta</h1>
+        <p>Espero que você pegue uma infecção fudida e morra seu verme miseravel</p>
+        <p>e tome no seu cu</p>
+    </div>
+    )
+}
+
+export default home;
